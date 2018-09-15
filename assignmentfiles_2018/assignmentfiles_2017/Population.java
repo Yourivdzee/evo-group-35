5e21fc2e8941efc3d6eea85a18adc80e5bec1ba0
Filip Knyszewski <filip_mateus96@hotmail.com>
2018-09-15 16:56:16 +0200

    added mating probability calculation functions and implemented roulette wheel and stochastic universal sampling methods for generating the mating pool

--- assignmentfiles_2018/assignmentfiles_2017/Population.java
+++ assignmentfiles_2018/assignmentfiles_2017/Population.java
@@ -1,8 +1,5 @@
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.*;
 import java.lang.Math;
-import java.util.Collections;
-import java.util.Comparator;
 
 public class Population{
     Integer size;
@@ -14,6 +11,8 @@ public class Population{
     ArrayList<Individual> matingPool;
 
     ArrayList<Individual> offsprings;
+
+    Random rand = new Random();
 
     public Population(Integer size){
         population = new ArrayList<>();
@@ -29,19 +28,34 @@ public class Population{
         return new ArrayList<>();
     }
 
+    /**
+     * Calculates and assigns the reproduction probability of each individual based
+     * on a linear model.
+     * @param s: parametrization values, should be 1<s<=2
+     */
+    public void calculateLinearReproductionProbability(double s){
+        for (int i = 0; i < size; i++)
+            population.get(i).selection_prob = (2 - s)/size + 2*i*(s-1)/(size*(size-1));
+    }
+
+
+    /**
+     * Calculates and assigns the reproduction probability of each individual based
+     * on an exponential model.
+     * @param c: normalisation factor, should be chosen so that the sum of probabilities
+     * is 1.
+     */
+    public void calculateExponentialReproductionProbability(double c){
+        for (int i = 0; i < size; i++)
+            population.get(i).selection_prob = (1- Math.pow(Math.E,-i))/c;
+    }
+
     public ArrayList<Individual> selectParentsByRank(String strategy, double s) {
         ArrayList<Individual> selection = new ArrayList<>();
 
         sortPopulationByFitness();
 
-        switch (strategy){
-            case "linear":
-                for (int i = 0; i < size; i++)
-                    population.get(i).selection_prob = (2 - s)/size + 2*i*(s-1)/(size*(size-1));
 
-            case "exp":
-                break;
-        }
 
         return selection;
 
@@ -85,6 +99,74 @@ public class Population{
 
         return stats;
     }
+
+    /**
+     * Calculates the cumulative reproduction probability of a population.
+     * Must be used after calculating the reproduction probabilities of each individual.
+     * @return ArrayList of doubles in which each index corresponds to the index in the
+     * population array.
+     */
+    private ArrayList<Double> calculateCumulativeReproductionProbability () {
+        ArrayList<Double> cumulativeProbability = new ArrayList<Double>();
+
+        double acc = 0;
+
+        try {
+            for (Individual individual : population) {
+                acc += individual.selection_prob;
+                cumulativeProbability.add(acc);
+            }
+        } catch (NullPointerException e) {
+            System.out.println("Please calculate the selection probability of the individuals before trying to obtain the cumulative reproduction probability distribution.");
+        }
+
+        assert (Math.abs(acc - 1) < 0.00001);
+
+        return cumulativeProbability;
+    }
+
+    /**
+     * Fills up the mating pool using the roulette wheel selection method.
+     * ALgorithm taken from the book.
+     */
+    public void rouletteWheel() {
+        int currentMember = 1;
+
+        ArrayList<Double> cumulativeProbability = calculateCumulativeReproductionProbability();
+
+        while (currentMember < size) {
+            double r = rand.nextGaussian();
+            int i = 1;
+            while (cumulativeProbability.get(i) < r)
+                i++;
+            matingPool.set(currentMember, population.get(i));
+        }
+
+    }
+
+    /**
+     * Fills up the mating pool using the stochastic universal sampling selection method.
+     * Algorithm taken from the book.
+     */
+    public void stochasticUniversalSampling() {
+        int currentMember = 1;
+        int i = 1;
+        int matingPoolSize = size;
+
+        ArrayList<Double> cumulativeProbability = calculateCumulativeReproductionProbability();
+
+        double r = rand.nextGaussian() * 1/matingPoolSize;
+
+        while (currentMember < matingPoolSize) {
+            while ( r < cumulativeProbability.get(i)){
+                matingPool.set(currentMember, population.get(i));
+                r = r + 1./matingPoolSize;
+                currentMember++;
+            }
+            i++;
+        }
+    }
+
 
 
     /**
