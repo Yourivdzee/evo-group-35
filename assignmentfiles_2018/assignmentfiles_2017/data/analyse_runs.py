c5180754b81a74efb27d77812102cd5c1393e6cc
Filip Knyszewski <filip_mateus96@hotmail.com>
2018-10-17 00:37:22 +0200

    added new scripts

--- assignmentfiles_2018/assignmentfiles_2017/data/analyse_runs.py
+++ assignmentfiles_2018/assignmentfiles_2017/data/analyse_runs.py
@@ -1,43 +1,58 @@
 import sys
 import os
 import math
+rootdir = os.getcwd()
+function = sys.argv[1]
+run_type =  sys.argv[2]
+rootdir2 = rootdir + "/{}/{}".format(function,run_type)
+save_loc = rootdir + "/" "{}_ofType_{}.txt".format(function, run_type)
+print(rootdir2)
+for foldername in os.listdir(rootdir2):
+    try:
+        scores = []
+        times = []
+        foldername = rootdir2 + "/" +foldername
+        for filename in os.listdir(foldername):
+            print(filename)
+            if filename.endswith(".txt"):
+                print("opened file {}".format(filename))
+                this_file = foldername + "/"+ filename
+                with open(this_file) as myfile:
+                    first_seven = list(myfile)[0:15]
+                with open(this_file) as myfile:
+                    last_two = list(myfile)[-2:]
+                    score = float(last_two[-2].split(" ")[1])
+                    time = float(last_two[-1].split(" ")[1].split("ms")[0])
+                    scores.append(score)
+                    times.append(time)
+                        
 
-directory = sys.argv[1]
-scores = []
-times = []
+        mean = sum(scores)/len(scores)
 
-for filename in os.listdir(directory):
-    if filename.endswith(".txt"):
-        with open(directory + "/" + filename) as myfile:
-            first_seven = list(myfile)[1:6]
-        with open(directory + "/" + filename) as myfile:
-            last_two = list(myfile)[-2:]
-            score = float(last_two[-2].split(" ")[1])
-            time = float(last_two[-1].split(" ")[1].split("ms")[0])
-            scores.append(score)
-            times.append(time)
-
-mean = sum(scores)/len(scores)
-
-sum_squares_scores = 0
-for score in scores:
-    sum_squares_scores += (mean - score)**2
+        sum_squares_scores = 0
+        for score in scores:
+            sum_squares_scores += (mean - score)**2
 
 
-mean_time = sum(times)/len(times)
-sum_squares_times = 0
-for time in times:
-    sum_squares_times += (mean_time - time)**2
-    
-with open("runs_summary.txt", "a") as summary_file:
-    summary_file.write("-------------------------------------\n")
-    for line in first_seven:
-        summary_file.write(line)
-    summary_file.write("Runs: {} \n".format(len(scores)))
-    summary_file.write("Mean fitness: {}\n".format(mean))
-    summary_file.write("Max fitness: {}\n".format(max(scores)))
-    summary_file.write("Standard deviation of scores: {} \n".format(math.sqrt(sum_squares_scores/len(scores))))
-    summary_file.write("Mean time: {}ms\n".format(mean_time))
-    summary_file.write("Standard deviaton of times: {}\n".format(math.sqrt(sum_squares_times/len(times))))
-
+        mean_time = sum(times)/len(times)
+        sum_squares_times = 0
+        for time in times:
+            sum_squares_times += (mean_time - time)**2
             
+        with open(save_loc, "a") as summary_file:
+            summary_file.write("-------------------------------------\n")
+            summary_file.write("Filename: " + foldername  + "\n")
+            for line in first_seven:
+                summary_file.write(line)
+
+            summary_file.write("Runs: {} \n".format(len(scores)))
+            summary_file.write("Mean fitness: {}\n".format(mean))
+            summary_file.write("Max fitness: {}\n".format(max(scores)))
+            summary_file.write("Standard deviation of scores: {} \n".format(math.sqrt(sum_squares_scores/len(scores))))
+            summary_file.write("Mean time: {}ms\n".format(mean_time))
+            summary_file.write("Standard deviaton of times: {}\n".format(math.sqrt(sum_squares_times/len(times))))
+
+    except (ValueError,IndexError, OSError,ZeroDivisionError) as err:
+        continue
+
+print("SAVING AT {}".format(save_loc))
